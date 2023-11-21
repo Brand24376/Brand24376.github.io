1b7bf9255d685dc4bed47c30fe02c66866d392c7
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-11-21 04:47:46 -0500

    Added my portfolio

--- main.js
+++ main.js
@@ -1,108 +1,167 @@
-import * as THREE from './three.module.js';
+import * as THREE from 'three';
 
+// Starting position of the images from the top
+const STARTY = 0;
+
+// Create a new scene
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+// Create and position the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = STARTY;
+camera.position.z = 30;
 
-const renderer = new THREE.WebGLRenderer(
-  { canvas: document.querySelector('#bg') },
-);
+// Create list of images in the 'img' folder
+let imgList = [
+    'photoOfMe.png',
+    'comptiaITF+Certification.png',
+    'OSHACertificate.png',
+    'UI_ShopCodeScreenshot.png',
+    'itemScriptCodeScreenshot.png',
+    'oreRandomizationSystemCodeScreenshot.png',
+    'ytecheduPhoto.png',
+    'yorkTechHonorRolls.png',
+    'oreMiningCodeScreenshot.png',
+    'myGithub.png',
+    'esportsIcon.png'
+];
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.y = 5;
-camera.position.z = 70;
+// Add every listed image as a plane mesh with texture to scene
+for (const image in imgList) {
+    // Every mesh has a geometry, texture, and material
+    // if else statement used if certain images require a different aspect ratio
+    if (image == 0) {
+        var geometry = new THREE.PlaneGeometry(20, 20);
+    }
+    if (image == 3) {
+        var geometry = new THREE.PlaneGeometry(30, 20);
+    }
+    if (image == 4) {
+        var geometry = new THREE.PlaneGeometry(20, 20);
+    }
+    if (image == 5) {
+        var geometry = new THREE.PlaneGeometry(20, 20);
+    }
+    if (image == 8) {
+        var geometry = new THREE.PlaneGeometry(30, 20);
+    }
+    if (image == 9) {
+        var geometry = new THREE.PlaneGeometry(30, 20);
+    }
+    if (image == 10) {
+        var geometry = new THREE.PlaneGeometry(15, 15);
+    }
+    if (image != 0 && image != 3 && image != 4 && image != 5 && image != 8 && image != 9 && image != 10) {
+        var geometry = new THREE.PlaneGeometry(20, 13.33);
+    }
+    const texture = new THREE.TextureLoader().load('img/' + imgList[image]);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+        map: texture // Add the texture image here
+    });
+    const plane = new THREE.Mesh(geometry, material);
 
-//LIGHTS
-const pointLight = new THREE.PointLight(0xFFFFFF, 1000, 1000);
-pointLight.position.set(0, 20, 50);
+    // Add the new plane to the scene
+    scene.add(plane);
+};
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+const background = new THREE.TextureLoader().load('img/background.png');
+scene.background = background;
 
-scene.add(pointLight);
-scene.add(ambientLight);
-
-//HELPERS
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-const axesHelper = new THREE.AxesHelper(20, 20, 20);
-scene.add(lightHelper);
-scene.add(gridHelper);
-scene.add(axesHelper);
-
-const geoPog = new THREE.CylinderGeometry(10, 10, 1.5, 32);
-const texturePog = new THREE.TextureLoader().load('vegetaUltraEgo.jpg');
-const matPog = new THREE.MeshStandardMaterial(
-  {
-    color: 0xFFFFFF,
-    wireframe: false,
-    map: texturePog
-  }
-);
-const pog = new THREE.Mesh(geoPog, matPog);
-pog.rotation.x = 45;
-
-scene.add(pog);
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xFFFFFF });
-  const star = new THREE.Mesh(geometry, material);
-
-  const x = THREE.MathUtils.randFloatSpread(400)
-  const y = THREE.MathUtils.randFloatSpread(300)
-  const z = THREE.MathUtils.randFloatSpread(100)
-  star.position.set(x, y - 25, z - 100);
-  scene.add(star);
-}
-
-Array(200).fill().forEach(addStar);
-
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
-
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.x = -10;
-
+// Move the camera with the scroll bar
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+    const top = document.body.getBoundingClientRect().top;
+    camera.position.y = STARTY + top * 0.07;
+};
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
-}
-
+// Add the scrollbar event to move camera
 document.body.onscroll = moveCamera;
 
-function animate(time) {
-  requestAnimationFrame(animate);
+// Resize the threejs canvas with the window and adjust for phone sizes
+function resizeWindow() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Adjust for phone or desktop size
+    if (window.innerWidth <= 600) {
+        camera.position.x = 0;
+        for (const child in scene.children) {
+            scene.children[child].rotation.y = 0;
+            scene.children[child].position.y = child * -50;
+        };
+    } else {
+        camera.position.x = 15;
+        for (const child in scene.children) {
+            scene.children[child].rotation.y = 15 * (Math.PI / 180);
+            scene.children[child].position.y = child * -30;
+            if (child == 1) {
+                scene.children[1].position.y = -93;
+                scene.children[1].position.x = -15.5;
+            }
+            if (child == 2) {
+                scene.children[2].rotation.y = -15 * (Math.PI / 180);
+                scene.children[2].position.y = -93;
+                scene.children[2].position.x = 4;
+            }
+            if (child == 3) {
+                scene.children[3].position.y = -27;
+                scene.children[3].position.x = -1;
+            }
+            if (child == 4) {
+                scene.children[4].position.y = -48;
+                scene.children[4].position.x = -15.5;
+            }
+            if (child == 5) {
+                scene.children[5].rotation.y = -15 * (Math.PI / 180);
+                scene.children[5].position.y = -48;
+                scene.children[5].position.x = 4;
+            }
+            if (child == 6) {
+                scene.children[6].position.y = -110;
+                scene.children[6].position.x = -15.5;
+            }
+            if (child == 7) {
+                scene.children[7].rotation.y = -15 * (Math.PI / 180);
+                scene.children[7].position.y = -110;
+                scene.children[7].position.x = 4;
+            }
+            if (child == 8) {
+                scene.children[8].position.y = -70;
+                scene.children[8].position.x = -1;
+            }
+            if (child == 9) {
+                scene.children[9].position.y = -135;
+                scene.children[9].position.x = -1;
+            }
+            if (child == 10) {
+                scene.children[10].position.y = -157;
+                scene.children[10].position.x = -1;
+            }
+        };
+    };
+};
 
-  pog.rotation.x += 0.01;
-  pog.rotation.y += 0.01;
-  pog.rotation.z += 0.01;
+// Resize canvas on window resize
+window.addEventListener('resize', resizeWindow, false);
 
-  renderer.render(scene, camera);
-}
+// Create the renderer and attach to the canvas
+const renderer = new THREE.WebGLRenderer(
+    { canvas: document.querySelector('#bg') }
+);
 
+// Set intital cavnas size
+resizeWindow();
+
+// Set renderer size and add it to the page
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Animation loop (calls itself recursively)
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+// Start the animation
 animate();
